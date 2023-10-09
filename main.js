690c01d543e4c8bb7fa756c79883cdf9fedc5d4b
PesukarhuTG <ganzhinovat@yandex.ru>
2023-10-09 12:08:54 +0300

    refactor: imports Swiper settings

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 import 'normalize.css';
 import './style.scss';
-import { Navigation, Thumbs } from 'swiper/modules';
-import Swiper from 'swiper';
-import 'swiper/css';
 
-const swiperThumbnails = new Swiper('.product__slider-thumbnails', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
+const productSlider = () => {
+  Promise.all([
+    import('swiper/modules'),
+    import('swiper'),
+    import('swiper/css'),
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    const swiperThumbnails = new Swiper.default('.product__slider-thumbnails', {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
 
-new Swiper('.product__slider-main', {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: '.product__arrow_next',
-    prevEl: '.product__arrow_prev',
-  },
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-  modules: [Navigation, Thumbs],
-});
+    new Swiper.default('.product__slider-main', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.product__arrow_next',
+        prevEl: '.product__arrow_prev',
+      },
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+      modules: [Navigation, Thumbs],
+    });
+  });
+};
+
+const init = () => {
+  productSlider();
+};
+
+init();
